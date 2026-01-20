74a4ca951676cc42d404570899ec7a73580be9c2
z-zlin <774016490@qq.com>
2026-01-20 12:30:30 +0800

    add ChatSessionHandler.h.cpp

---
+++ AIApps/ChatServer/src/handlers/ChatSessionsHandler.cpp
@@ -0,0 +1,80 @@
+#include "../include/handlers/ChatSessionsHandler.h"
+
+
+void ChatSessionsHandler::handle(const http::HttpRequest& req, http::HttpResponse* resp)
+{
+    try
+    {
+
+        auto session = server_->getSessionManager()->getSession(req, resp);
+        LOG_INFO << "session->getValue(\"isLoggedIn\") = " << session->getValue("isLoggedIn");
+        if (session->getValue("isLoggedIn") != "true")
+        {
+
+            json errorResp;
+            errorResp["status"] = "error";
+            errorResp["message"] = "Unauthorized";
+            std::string errorBody = errorResp.dump(4);
+
+            server_->packageResp(req.getVersion(), http::HttpResponse::k401Unauthorized,
+                "Unauthorized", true, "application/json", errorBody.size(),
+                errorBody, resp);
+            return;
+        }
+
+
+        int userId = std::stoi(session->getValue("userId"));
+        std::string username = session->getValue("username");
+        
+        std::vector<std::string> sessions;  
+
+        {
+            std::lock_guard<std::mutex> lock(server_->mutexForSessionsId);
+            sessions = server_->sessionsIdsMap[userId]; 
+        }
+
+        json successResp;
+        successResp["success"] = true;
+
+
+        json sessionArray = json::array();
+        for (auto sid : sessions) {
+            json s;
+            s["sessionId"] = sid;
+            s["name"] = "Ự " + sid;
+            sessionArray.push_back(s);
+        }
+        successResp["sessions"] = sessionArray;
+
+        std::string successBody = successResp.dump(4);
+
+        resp->setStatusLine(req.getVersion(), http::HttpResponse::k200Ok, "OK");
+        resp->setCloseConnection(false);
+        resp->setContentType("application/json");
+        resp->setContentLength(successBody.size());
+        resp->setBody(successBody);
+        return;
+    }
+    catch (const std::exception& e)
+    {
+
+        json failureResp;
+        failureResp["status"] = "error";
+        failureResp["message"] = e.what();
+        std::string failureBody = failureResp.dump(4);
+        resp->setStatusLine(req.getVersion(), http::HttpResponse::k400BadRequest, "Bad Request");
+        resp->setCloseConnection(true);
+        resp->setContentType("application/json");
+        resp->setContentLength(failureBody.size());
+        resp->setBody(failureBody);
+    }
+}
+
+
+
+
+
+
+
+
+
